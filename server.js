249c9ed63fbab5efc3106ff7517e8660386c053e
Alejandro <alesantin012@hotmail.com>
2021-01-18 06:12:48 -0300

    partials

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const port = process.env.PORT || 3000
 
 //helper
 hbs.registerHelper('year', () => {
-        return new Date().getFullYear()
-    })
-    // hbs.registerPartials(__dirname + '/views')
+    return new Date().getFullYear()
+})
 
 app.use(express.static(__dirname + ''))
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
